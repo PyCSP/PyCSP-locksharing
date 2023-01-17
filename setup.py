b3b04a6b3bb20b571b24e81071a20c2b1c04b80d
John Markus Bjørndalen <jmb@cs.uit.no>
2023-01-17 16:29:56 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# see http://docs.python.org/dist/dist.html
+#
+from distutils.core import setup
+
+
+setup(name='pycsp',
+      version='0.2.0',
+      description='PyCSP - Python CSP Library',
+      author='John Markus Bjørndalen',
+      author_email='jmb@cs.uit.no',
+      url='https://github.com/PyCSP/PyCSP-locksharing',
+      license='MIT',
+      packages=['pycsp', 'pycsp.plugNplay'],
+      platforms=['any'],
+      )
